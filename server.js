dd093199e944338f9cb3bd204aaaac2a464cdf62
TyQuincia-Oby <tyquinciaoby@gmail.com>
2025-09-26 23:34:29 +0000

    update

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express ();
+const PORT = 3000;
+
+//Home route
+app.get('/', (req, res) => {
+    res.send (
+        `Hello World!`
+    )
+});
+
+//Event Listener
+app.listen(PORT,() => {
+    console.log(`Server is running on:` + PORT);
+}
+
+)
